1c27e2aeab47355ce5275850a429355dba14d512
Bost <thebost@gmail.com>
2019-02-12 13:39:40 +0100

    Upgrade deps, lein-virgil, lein-v

--- project.clj
+++ project.clj
@@ -1,3 +1,14 @@
-(defproject delimc "0.2.0"
+(defproject delimc :lein-v
   :description "Delimited Continuations for Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies
+  [[org.clojure/clojure "1.10.0"]
+   ]
+  :plugins
+  [
+   ;; autorecompile changed java files
+   [lein-virgil "0.1.9"]
+
+   ;; drive leiningen project version from git
+   [com.roomkey/lein-v "7.0.0"]
+   ]
+  )
